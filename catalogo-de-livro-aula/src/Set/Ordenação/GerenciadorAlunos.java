fc377ae59f5315c3d5a0bff1b15470d36ebe81a1
ElisaAlves2024 <alveselisarodrigues@gmail.com>
2024-06-01 12:38:50 +0200

    atualizações no exercicio

--- catalogo-de-livro-aula/src/Set/Ordenação/GerenciadorAlunos.java
+++ catalogo-de-livro-aula/src/Set/Ordenação/GerenciadorAlunos.java
@@ -2,13 +2,62 @@ package Set.Ordenação;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class GerenciadorAlunos {
 
-    public Set<String> alunosSet;
+    public Set<Aluno> alunoSet;
 
     public GerenciadorAlunos() {
-        this.alunosSet = new HashSet<>();
+        this.alunoSet = new HashSet<>();
+    }
+
+    public void adicionarAluno(String nome, long matricula, double nota) {
+        alunoSet.add(new Aluno(nome, matricula, nota));
+    }
+    
+    public void removerAluno(long matricula) {
+        Aluno alunoParaRemover = null;
+        if (!alunoSet.isEmpty()) {
+            for (Aluno a : alunoSet) {
+                if (a.getMatricula() == matricula) {
+                alunoParaRemover = a;
+                break;
+                }           
+            }
+            alunoSet.remove(alunoParaRemover);
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+  
+        if (alunoParaRemover == null) {
+            System.out.println("Matricula não encontrada!");
+        }
+    }
+
+    public void exibirAlunosPorNome() {
+        Set<Aluno> alunoPorNome = new TreeSet<>(alunoSet);
+        if (!alunoSet.isEmpty()) {
+            System.out.println(alunoPorNome);          
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+    
+    public void exibirAlunosPorNota() {
+        Set<Aluno> alunoPorNota = new TreeSet<>(new ComparatorNota());
+        if (!alunoSet.isEmpty()) {
+            alunoPorNota.addAll(alunoSet);
+            System.out.println(alunoPorNota);           
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    
+    }
+
+
+    public void exibirAlunos() {
+        System.out.println(alunoSet);
     }
 
 
